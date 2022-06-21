754326f5bb1cbf7aa929295a29ce7612d8334666
carlton <carltonmbaja0@gmail.com>
2022-06-21 04:59:11 -0700

    Added input fields to capture employee details

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
-// fetch("http://ip-api.com/json/{query}")
-// .then((response) => response.json())
-// .then(data => {
-//     console.log(data)
-// })
-// fetch('https://ipapi.co/8.8.8.8/json/')
-// .then(function(response) {
-//   response.json().then(jsonData => {
-//     console.log(jsonData);
-//   });
-// })
-// .catch(function(error) {
-//   console.log(error)
-// });
 
-fetch('https://ipapi.co/json/')
-.then(function(response) {
-  response.json().then(jsonData => {
-    Ip.innerHTML = jsonData.ip
-    version.innerHTML = jsonData.version
-    city.innerHTML = jsonData.city
-    region.innerHTML = jsonData.region
-    console.log(jsonData);
-  });
-})
-// .catch(function(error) {
-//   console.log(error)
-// });
+//Obtaining html emelemnts using getElementById() method
 const Ip = document.getElementById("Ip")
-const version = document.getElementById("version")
+// const version = document.getElementById("version")
 const city = document.getElementById("city")
 const region = document.getElementById("region")
+// const region_code = document.getElementById("region_code")
+const country_calling_code = document.getElementById("country_calling_code")
+const latitude = document.getElementById("latitude")
+const country = document.getElementById("country")
 
+//Fetching data from the API then converting it into JSON
+fetch('https://ipapi.co/json/')
+  .then(function (response) {
+    response.json().then(jsonData => {
+      //Parsing the data into html with specific id's
+      Ip.innerHTML = jsonData.ip
+      // version.innerHTML = jsonData.version
+      country.innerHTML = jsonData.country_name
+      city.innerHTML = jsonData.city
+      region.innerHTML = jsonData.region
+      // region_code.innerHTML = jsonData.region_code
+      country_calling_code.innerHTML = jsonData.country_calling_code
+      latitude.innerHTML = jsonData.latitude
 
-// function renderIp(data){
-// let ipAdress = ""
-// console.log(data);
-// }
+      console.log(jsonData);
+    });
+  })
+  .catch(function (error) {
+    console.log(error)
+  });
