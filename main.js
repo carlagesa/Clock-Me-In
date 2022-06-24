8ad0975f140507cf9d04048121898817125ee5b1
carlton <carltonmbaja0@gmail.com>
2022-06-24 03:19:02 -0700

    added a mini dashboard to the project

--- main.js
+++ main.js
@@ -52,19 +52,19 @@ fetch(
   });
   
 
-document.getElementById("submit").addEventListener("click", submit);
-document.addEventListener("DOMContentLoaded", submit);
+// document.getElementById("submit").addEventListener("click", submit);
+// document.addEventListener("DOMContentLoaded", submit);
 
-setInterval(() => {
-  if (
-    document.getElementById("username").value == ""
-    // document.getElementById("password").value == ""
-  ) {
-    document.getElementById("submit").setAttribute("disabled", "");
-  } else {
-    document.getElementById("submit").removeAttribute("disabled");
-  }
-});
+// setInterval(() => {
+//   if (
+//     document.getElementById("username").value == ""
+//     // document.getElementById("password").value == ""
+//   ) {
+//     document.getElementById("submit").setAttribute("disabled", "");
+//   } else {
+//     document.getElementById("submit").removeAttribute("disabled");
+//   }
+// });
 
 // function check(form) {
 //   if (form.username.value && form.password.value) {
@@ -74,9 +74,44 @@ setInterval(() => {
 //   }
 // }
 
-document.getElementById("submit").onclick = function submit() {
-  // window.location.href = "../../menu.html";
-  var username = document.getElementById("username").value;
+// document.getElementById("submit").onclick = function submit() {
+//   // window.location.href = "../../menu.html";
+//   var username = document.getElementById("username").value;
 
-  alert(username + " " + "clocked in!");
-};
+//   alert(username + " " + "clocked in!");
+// };
+// new lines
+
+document.addEventListener("DOMContentLoaded", () => {
+  // your code here
+  const toDoContainer = document.querySelector("#tasks");
+  const newTask = document.querySelector("#new-task-description");
+  const worker = document.getElementById("workerId");
+  const form = document.querySelector("#create-task-form");
+
+  const isValidInput = (input) => {
+    input = input.trim();
+    return input ? input : false;
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+    getTask();
+  };
+
+  const getTask = () => {
+    const task = newTask.value;
+    if (isValidInput(task)) {
+      createTask(task);
+    }
+  };
+
+  const createTask = (taskContent) => {
+    const task = document.createElement("li");
+    task.textContent = taskContent;
+    toDoContainer.appendChild(task);
+    form.reset();
+  };
+
+  form.addEventListener("submit", addTask);
+});
